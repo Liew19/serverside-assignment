7c1be717e3c7bd4cac12b766aa115bd019953166
Xin Ai <xinai021128@gmail.com>
2025-04-17 03:03:30 +0800

    add comment (done)
    can add comment and retrieve comment now

--- php/community/api/comment.php
+++ php/community/api/comment.php
@@ -2,6 +2,17 @@
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  // Explicitly return headers in preflight response
+  header("Access-Control-Allow-Origin: http://localhost:3000");
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type");
+  header("Access-Control-Allow-Credentials: true");
+  http_response_code(200);
+  exit;
+}
 
 require_once __DIR__ . '/../../database/database.php';
 require_once '../models/comment.php';
