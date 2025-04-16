b611ecefc528f1886488ec438c2e570a9a3ecf9e
Ooi Guan Cheng <ooiguancheng1118@1utar.my>
2025-04-16 16:01:05 +0800

    Update admin.php

--- php/competition/api/admin.php
+++ php/competition/api/admin.php
@@ -9,7 +9,7 @@ header("Access-Control-Allow-Credentials: true"); // Allow credentials (cookies,
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Cache-Control, Pragma");
 
 session_start();
-$conn = new mysqli("localhost", "root", "password", "database_test2");
+$conn = new mysqli("localhost", "root", "", "recipe_database");
 
 $headers = getallheaders();
 
